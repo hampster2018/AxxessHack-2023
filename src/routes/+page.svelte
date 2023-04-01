59c160b3a15c840214deed45686f57e0ff08e8cf
Nicholas Zolton <nicholaszolton+git@gmail.com>
2023-04-01 11:23:48 -0500

    Added boilerplate prettier and css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Homepage</title>
+	<link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css" />
+</svelte:head>
+
+<main>
+	<h1>Welcome to our project!</h1>
+</main>
+
+<style lang="scss">
+	:global(body),
+	:global(html) {
+		margin: 0;
+		padding: 0;
+	}
+
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
