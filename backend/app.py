77246578084d712ce5d1014ed2fb01da848eec54
Naomi Zilber <541664@gmail.com>
2023-04-01 16:38:58 -0500

    backend changes

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def helloWorld():
-    return "<p>Hello World!</p>"
+@app.route("/", methods=['POST'])
+def getData():
+    # info needed by hospital/doctor
+    hospInfo = ["f_name", "mi", "l_name", "dob", "phone_num", "ss", "address", "city", "state", "zip", "sex"]
+
+    # get the info from the client
+    if request.method == 'POST':
+        f_name = request.form.get('f_name')
+        mi = request.form.get('MI')
+        l_name = request.form.get('l_name')
+        dob = request.form.get('dob')
+        phone_num = request.form.get('phone_num')
+        ss = request.form.get('SS')
+        address = request.form.get('address')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        zip = request.form.get('zip')
+        sex = request.form.get('sex')
+
+    clientInfo = [f_name, mi, l_name, dob, phone_num, ss, address, city, state, zip, sex]
+
+    # information not provided by client but still needed by hospital
+    infoNeeded = []
+
+    # check that hospital has all of the info they need
+    for item in hospInfo:
+        if item not in clientInfo.keys():
+            infoNeeded.append(item)
