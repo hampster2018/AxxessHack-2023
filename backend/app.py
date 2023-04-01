305688d2b00f4f4177dfc75555cf3a9f0e15c168
Nicholas Zolton <nicholaszolton+git@gmail.com>
2023-04-01 17:28:01 -0500

    Added generate_prompt function

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,43 @@
+import os
 from flask import Flask, request
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
+
+
+@app.route("/summary", methods=['GET'])
+def getSummary():
+    if request.method == 'GET':
+        params = request.args
+        os.getenv('OPENAI_APIKEY')
+        print(params['summary'])
+        return "summary printed: " + str(params)
+    return "<p>error</p>"
+
+
+def generate_prompt(drug_name, drug_info):
+    # this will basically grab the drug info and then generate a prompt with it so that the chatbot can use
+    # it to answer questions
+    return """
+    The following is a conversation with a doctor about a patient's prescription. The doctor is you.
+    Patient: I have a prescription for a drug called {}, here is the information about the drug: {}.
+    Patient: Could you please answer some questions about the drug for me? Doctor: Sure, what would you like to know?
+    Patient: What is a summary of the side effects?
+    """.format(drug_name, drug_info)
+
+
+def prompt_chatbot(prompt):
+    # this will take the prompt and then use the chatbot to generate a response
+    # this will return a string of the response
+    return "This is the response"
+
 
 @app.route("/", methods=['POST'])
 def getData():
     # info needed by hospital/doctor
-    hospInfo = ["f_name", "mi", "l_name", "dob", "phone_num", "ss", "address", "city", "state", "zip", "sex"]
+    hospInfo = ["f_name", "mi", "l_name", "dob", "phone_num",
+                "ss", "address", "city", "state", "zip", "sex"]
 
     # get the info from the client
     if request.method == 'POST':
@@ -21,7 +53,8 @@ def getData():
         zip = request.form.get('zip')
         sex = request.form.get('sex')
 
-    clientInfo = [f_name, mi, l_name, dob, phone_num, ss, address, city, state, zip, sex]
+    clientInfo = [f_name, mi, l_name, dob, phone_num,
+                  ss, address, city, state, zip, sex]
 
     # information not provided by client but still needed by hospital
     infoNeeded = []
