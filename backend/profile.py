f4b126c7d6d3de13ba1146de418192987a6ddffa
Naomi Zilber <541664@gmail.com>
2023-04-01 19:52:50 -0500

    profile info

---
+++ backend/profile.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def getProfile():
+    f_name = request.form.get('f_name')
+    l_name = request.form.get('l_name')
+    dob = request.form.get('dob')
+    sex = request.form.get('sex')
+    insurance = request.form.get('insurance')
+    bloodtype = request.form.get('bloodtype')
+    height = request.form.get('height')
+    weight = request.form.get('weight')
+    meds = request.form.get('meds')
