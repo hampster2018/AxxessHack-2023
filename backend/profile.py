c2e13ba0d4c682b0bf231ab7001f66daf7dcf7da
Naomi Zilber <541664@gmail.com>
2023-04-01 21:23:53 -0500

    user info updated

--- backend/profile.py
+++ backend/profile.py
@@ -4,12 +4,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def getProfile():
-    f_name = request.form.get('f_name')
-    l_name = request.form.get('l_name')
-    dob = request.form.get('dob')
-    sex = request.form.get('sex')
-    insurance = request.form.get('insurance')
-    bloodtype = request.form.get('bloodtype')
-    height = request.form.get('height')
-    weight = request.form.get('weight')
-    meds = request.form.get('meds')
+    if request.method == 'POST':
+        f_name = request.form.get('f_name')
+        l_name = request.form.get('l_name')
+        dob = request.form.get('dob')
+        sex = request.form.get('sex')
+        insurance = request.form.get('insurance')
+        bloodtype = request.form.get('bloodtype')
+        height = request.form.get('height')
+        weight = request.form.get('weight')
+        meds = request.form.get('meds')
+
